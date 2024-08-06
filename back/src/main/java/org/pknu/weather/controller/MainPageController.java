f33d5772ee7dec73fb2c542f482af2cd42820de9
bbangHo <sonsins1@naver.com>
2024-08-06 15:26:49 +0900

    [config] OpenFegin 설정 및 관련 클래스 생성, 테스트
    [feat] 날씨 공공 api 관련 클래스 및 테스트 생성
    
    - 위도/경도를 nx, ny 좌표로 치환해 주는 유틸 클래스 생성CoordinateConversionUtils
    - 현재 날짜를 yyyyMMdd로 시간은 HHmm으로 변경해주는 유틸 클래스 생성 DateTimeFormatUtils
    - 메인 화면에서 다룰 API를 관리하는 컨트롤러 생성 MainPageApiService
    - OpenFegin을 사용해서 날씨 데이터를 받아오는 인터페이스, 테스트 생성 WeatherFeignClientTest

---
+++ back/src/main/java/org/pknu/weather/controller/MainPageController.java
@@ -0,0 +1,15 @@
+package org.pknu.weather.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/main")
+public class MainPageController {
+
+
+}
