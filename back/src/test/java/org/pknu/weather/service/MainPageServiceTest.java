dd99374ad9c423f357d4dc158cca28b2e32d15e5
bbangHo <sonsins1@naver.com>
2024-08-19 01:23:18 +0900

    [fix] MainPageServiceTest 테스트 코드 수정

--- back/src/test/java/org/pknu/weather/service/MainPageServiceTest.java
+++ back/src/test/java/org/pknu/weather/service/MainPageServiceTest.java
@@ -92,7 +92,7 @@ class MainPageServiceTest {
 
         // then
         assertThat(weatherInfo.getCurrentTmp()).isEqualTo(weatherList.get(0).getTemperature());
-        assertThat(weatherInfo.getWeatherPerHourList().size()).isGreaterThanOrEqualTo(22);  // 3시간 간격으로 예보를 발표하기 때문에 22 ~ 24개의 예보
+        assertThat(weatherInfo.getWeatherPerHourList().size()).isGreaterThanOrEqualTo(21);  // 3시간 간격으로 예보를 발표하기 때문에 22 ~ 24개의 예보
         assertThat(weatherInfo.getCurrentSkyType()).isEqualTo(weatherList.get(0).getSkyType());
     }
 }
