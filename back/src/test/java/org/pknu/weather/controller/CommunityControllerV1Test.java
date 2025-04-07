62119a4329923c029c327e7bbe9085d6048f6ede
bbangHo <sonsins1@naver.com>
2025-04-08 00:27:48 +0900

    test: 의존성 주입 문제로 통합 테스트로 변경

--- back/src/test/java/org/pknu/weather/controller/CommunityControllerV1Test.java
+++ back/src/test/java/org/pknu/weather/controller/CommunityControllerV1Test.java
@@ -1,59 +1,37 @@
 package org.pknu.weather.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.pknu.weather.common.TestDataCreator;
 import org.pknu.weather.common.TestUtil;
-import org.pknu.weather.common.mapper.EnumTagMapper;
-import org.pknu.weather.domain.Location;
 import org.pknu.weather.domain.Member;
 import org.pknu.weather.domain.Post;
-import org.pknu.weather.domain.common.PostType;
-import org.pknu.weather.repository.LocationRepository;
 import org.pknu.weather.repository.MemberRepository;
 import org.pknu.weather.repository.PostRepository;
-import org.pknu.weather.repository.RecommendationRepository;
 import org.pknu.weather.security.util.JWTUtil;
-import org.pknu.weather.service.PostQueryService;
 import org.pknu.weather.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
-@WebMvcTest(CommunityControllerV1.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class CommunityControllerV1Test {
-    @MockBean
+    @Autowired
     PostRepository postRepository;
 
-    @MockBean
-    LocationRepository locationRepository;
-
-    @MockBean
+    @Autowired
     MemberRepository memberRepository;
 
-    @MockBean
-    RecommendationRepository recommendationRepository;
-
-    @MockBean
-    EnumTagMapper enumTagMapper;
-
-    PostQueryService postQueryService;
-
-    @InjectMocks
+    @Autowired
     PostService postService;
 
     @Autowired
@@ -62,25 +40,12 @@ class CommunityControllerV1Test {
     @Autowired
     JWTUtil jwtUtil;
 
-    @BeforeEach
-    void setUp() {
-        postQueryService = Mockito.spy(new PostQueryService(postRepository, memberRepository, postService));
-    }
-
     @Test
+    @Transactional
     void 게시글조회성공_작성자정보와레벨함꼐반환() throws Exception {
         // given
-        Member member = Member.builder()
-                .id(1L)
-                .location(TestDataCreator.getBusanLocation())
-                .email("test@naver.com")
-                .build();
-
-        Member member2 = Member.builder()
-                .id(2L)
-                .location(TestDataCreator.getBusanLocation())
-                .email("test2@naver.com")
-                .build();
+        Member member = memberRepository.save(TestDataCreator.getBusanMember("test1"));
+        Member member2 = memberRepository.save(TestDataCreator.getBusanMember("test2"));
 
         Post post = TestDataCreator.getPost(member);
         Post post2 = TestDataCreator.getPost(member2);
@@ -90,13 +55,7 @@ class CommunityControllerV1Test {
         postList.add(post2);
 
         String jwt = TestUtil.generateJwtToken(jwtUtil, member);
-
-        when(memberRepository.findByEmail(any(String.class))).thenReturn(Optional.of(member));
-        when(memberRepository.safeFindByEmail(any(String.class))).thenReturn(member);
-        when(memberRepository.safeFindById(any(Long.class))).thenReturn(member);
-        when(locationRepository.safeFindById(any(Long.class))).thenReturn(member.getLocation());
-        when(postRepository.findAllWithinDistance(any(Long.class), any(Long.class), any(Location.class),
-                any(PostType.class))).thenReturn(postList);
+        postRepository.saveAll(postList);
 
         // when
         ResultActions result = mockMvc.perform(get("/api/v1/community/posts")
@@ -105,12 +64,9 @@ class CommunityControllerV1Test {
         // then
         result
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.result.postList[0].memberInfo.memberName").value(member.getNickname()))
-                .andExpect(jsonPath("$.result.postList[0].memberInfo.levelName").value(member.getLevel().name()))
+                .andExpect(jsonPath("$.result.postList[0].memberInfo.memberName").value(member2.getNickname()))
+                .andExpect(jsonPath("$.result.postList[0].memberInfo.levelName").value(member2.getLevel().name()))
                 .andExpect(jsonPath("$.result.postList[1].memberInfo.memberName").value(member.getNickname()))
                 .andExpect(jsonPath("$.result.postList[1].memberInfo.levelName").value(member.getLevel().name()));
-
-        verify(postService).getPosts(any(Long.class), any(Long.class), any(Long.class), any(String.class),
-                any(Long.class));
     }
 }
--- back/src/test/java/org/pknu/weather/controller/MemberControllerV2Test.java
+++ back/src/test/java/org/pknu/weather/controller/MemberControllerV2Test.java
@@ -1,41 +1,27 @@
 package org.pknu.weather.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.pknu.weather.common.TestDataCreator;
 import org.pknu.weather.common.TestUtil;
-import org.pknu.weather.common.utils.LocalUploaderUtils;
 import org.pknu.weather.domain.Member;
 import org.pknu.weather.domain.exp.Level;
 import org.pknu.weather.repository.MemberRepository;
 import org.pknu.weather.security.util.JWTUtil;
-import org.pknu.weather.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MemberControllerV2Test {
-    @InjectMocks
-    MemberService memberService;
-
-    @MockBean
+    @Autowired
     MemberRepository memberRepository;
-
-    @MockBean
-    LocalUploaderUtils localUploaderUtils;
 
     @Autowired
     MockMvc mockMvc;
@@ -46,13 +32,8 @@ public class MemberControllerV2Test {
     @Test
     void 사용자정보조회성공() throws Exception {
         // given
-        Member member = Member.builder()
-                .id(1L)
-                .location(TestDataCreator.getBusanLocation())
-                .email("test@naver.com")
-                .build();
+        Member member = memberRepository.save(TestDataCreator.getBusanMember());
         String jwt = TestUtil.generateJwtToken(jwtUtil, member);
-        when(memberRepository.findMemberByEmail(any(String.class))).thenReturn(Optional.of(member));
 
         // when
         ResultActions result = mockMvc.perform(get("/api/v2/member/info")
@@ -68,7 +49,5 @@ public class MemberControllerV2Test {
                 .andExpect(jsonPath("$.result.exp").value(member.getExp()))
                 .andExpect(jsonPath("$.result.nextLevelRequiredExp").value(
                         Level.getNextLevel(member.getLevel()).getRequiredExp()));
-
-        verify(memberService).findFullMemberInfoByEmail(any(String.class));
     }
 }
