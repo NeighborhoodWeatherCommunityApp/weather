51a44d599a754c9605994ef2de243ae627e56856
bbangHo <sonsins1@naver.com>
2025-04-16 08:08:55 +0900

    style: 이벤트 구분 코드 추가

--- back/src/main/java/org/pknu/weather/domain/exp/ExpEvent.java
+++ back/src/main/java/org/pknu/weather/domain/exp/ExpEvent.java
@@ -6,15 +6,16 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ExpEvent {
-    CREATE_POST(10L),   // 게시글 작성
-    ATTENDANCE(1L),     // 출석체크
-    STREAK_7_DAYS(15L), // 7연속 출석체크
-    RECOMMEND(1L),     // 좋아요 클릭
-    RECOMMENDED(3L),  // 좋아요 받음
-    SHARE_KAKAO(5L),    // 카카오로 날씨 공유
-    INACTIVE_7_DAYS(-5L),   // 7일 미출석
-    INACTIVE_30_DAYS(-25L)  // 30일 미출석
-    ;
+    CREATE_POST("게시글 작성", 10L, false),
+    ATTENDANCE("출석 체크", 1L, true),
+    STREAK_7_DAYS("7일 연속 출석 체크", 15L, false),
+    RECOMMEND("좋아요 클릭", 1L, false),
+    RECOMMENDED("좋아요 받음", 3L, false),
+    SHARE_KAKAO("카카오톡 날씨 공유", 5L, true),
+    INACTIVE_7_DAYS("7일 미출석", -5L, false),
+    INACTIVE_30_DAYS("30일 미출석", -25L, false);
 
+    private final String rewardName;
     private final Long rewardExpAmount;
+    private final Boolean allowApiRequest;
 }
