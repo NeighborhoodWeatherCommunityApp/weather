c320651da912a230ea2a628adc022138605b0d34
bbangHo <sonsins1@naver.com>
2025-05-29 06:34:38 +0900

    chore: 테스트 수정으로 애노테이션 삭제

--- back/src/main/java/org/pknu/weather/config/AsyncConfig.java
+++ back/src/main/java/org/pknu/weather/config/AsyncConfig.java
@@ -1,7 +1,6 @@
 package org.pknu.weather.config;
 
 import java.util.concurrent.Executor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,7 +11,6 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class AsyncConfig {
 
     @Bean(name = "WeatherCUDExecutor")
-    @ConditionalOnMissingBean(name = "WeatherCUDExecutor")
     public Executor getWeatherAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
@@ -24,7 +22,6 @@ public class AsyncConfig {
     }
 
     @Bean(name = "ExpCUDExecutor")
-    @ConditionalOnMissingBean(name = "ExpCUDExecutor")
     public Executor getExpAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(1);
