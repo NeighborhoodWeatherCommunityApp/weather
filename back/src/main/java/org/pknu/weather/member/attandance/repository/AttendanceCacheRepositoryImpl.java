9ef2ef8b86d71ebeeb2c50c11dfbd03e3c592176
bbangHo <sonsins1@naver.com>
2025-09-17 18:55:04 +0900

    fix: NPE를 대비하여 수정

--- back/src/main/java/org/pknu/weather/member/attandance/repository/AttendanceCacheRepositoryImpl.java
+++ back/src/main/java/org/pknu/weather/member/attandance/repository/AttendanceCacheRepositoryImpl.java
@@ -2,11 +2,14 @@ package org.pknu.weather.member.attandance.repository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.pknu.weather.apipayload.code.status.ErrorStatus;
+import org.pknu.weather.exception.GeneralException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
@@ -15,12 +18,16 @@ public class AttendanceCacheRepositoryImpl implements AttendanceCacheRepository 
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    @Override
     public boolean checkIn(String key, Long userId) {
         return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().setBit(key, userId, true));
     }
 
     @Override
     public boolean checkIn(DefaultRedisScript<Long> script, String key, Long userId) {
-        return stringRedisTemplate.execute(script, List.of(key), String.valueOf(userId)) != 0L;
+        Long result = Optional.ofNullable(stringRedisTemplate.execute(script, List.of(key), String.valueOf(userId)))
+                .orElseThrow(() -> new GeneralException(ErrorStatus._REDIS_INTERNAL_SERVER_ERROR));
+
+        return result.equals(1L);
     }
 }
