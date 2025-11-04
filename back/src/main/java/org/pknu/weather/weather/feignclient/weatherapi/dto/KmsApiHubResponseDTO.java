0e80f4f64758f3ed64c885d29f9f179bc3e820c9
bbangHo <sonsins1@naver.com>
2025-11-04 21:00:26 +0900

    .

--- back/src/main/java/org/pknu/weather/weather/feignclient/weatherapi/dto/KmsApiHubResponseDTO.java
+++ back/src/main/java/org/pknu/weather/weather/feignclient/weatherapi/dto/KmsApiHubResponseDTO.java
@@ -13,53 +13,53 @@ import lombok.NoArgsConstructor;
 public class KmsApiHubResponseDTO {
 
     @JsonProperty("header")
-    private Header response;
+    private Response response;
 
     @Getter
     @NoArgsConstructor
-    public static class response {
+    public static class Response {
         @JsonProperty("header")
         private Header header;
 
         @JsonProperty("body")
         private Body body;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class Header {
-        @JsonProperty("resultCode")
-        private Integer resultCode;
-
-        @JsonProperty("resultMsg")
-        private String resultMsg;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    public static class Body {
-
-        @JsonProperty("dataType")
-        private String dataType;
-
-        @JsonProperty("items")
-        private Items items;
-
-        @JsonProperty("pageNo")
-        private int pageNo;
-
-        @JsonProperty("numOfRows")
-        private int numOfRows;
-
-        @JsonProperty("totalCount")
-        private int totalCount;
 
         @Getter
         @NoArgsConstructor
-        public static class Items {
+        public static class Header {
+            @JsonProperty("resultCode")
+            private Integer resultCode;
 
-            @JsonProperty("item")
-            private List<Item> itemList;
+            @JsonProperty("resultMsg")
+            private String resultMsg;
+        }
+
+        @Getter
+        @NoArgsConstructor
+        public static class Body {
+
+            @JsonProperty("dataType")
+            private String dataType;
+
+            @JsonProperty("items")
+            private Items items;
+
+            @JsonProperty("pageNo")
+            private int pageNo;
+
+            @JsonProperty("numOfRows")
+            private int numOfRows;
+
+            @JsonProperty("totalCount")
+            private int totalCount;
+
+            @Getter
+            @NoArgsConstructor
+            public static class Items {
+
+                @JsonProperty("item")
+                private List<Item> itemList;
+            }
         }
     }
 }
